68013011561cef98158a8085984b81efa34a4ee3
Lakshman76 <lakshmankumar2603@gmail.com>
2025-07-01 17:00:36 +0530

    Handling multiple nested route

--- src/app.js
+++ src/app.js
@@ -1,20 +1,65 @@
-const express = require('express');
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get("/user", (req, res) => {
-    res.send({ firstName: "Lakshman", lastName: "Kumar" });
-})
+// app.use("/route", rH1, rH2, rH3, rH4, rH5);
+app.use(
+    "/user",
+    (req, res, next) => {
+        console.log("Route handlers");
+        // res.send("Response!!!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("2nd route handlers!");
+        // res.send("2nd Respose!!")
+        next();
+    },
+    (req, res, next) => {
+        console.log("3rd route handlers!");
+        // res.send("3rd Respose!!")
+        next();
+    },
+    (req, res, next) => {
+        console.log("4th route handlers!");
+        // res.send("4th Respose!!")
+        next();
+    }
+);
 
-app.post("/user", (req, res) => {
-    console.log("Data saved successfully");
-    res.send("Data saved successfully to the database");
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Data deleted successfully from the database");
-})
-
+// app.use("/route", [rH1, rH2, [rH3, rH4], rH5]);
+app.use(
+    "/user",
+    (req, res, next) => {
+        console.log("new route handlers!");
+        // res.send("new Respose!!");
+        next();
+    },
+    [
+        (req, res, next) => {
+            console.log("new 2nd route handlers!");
+            // res.send("new 2nd Respose!!");
+            next();
+        },
+        [
+            (req, res, next) => {
+                console.log("new 3rd route handlers!");
+                // res.send("new 3rd Respose!!");
+                next();
+            },
+            (req, res, next) => {
+                console.log("new 4th route handlers!");
+                // res.send("new 4th Respose!!");
+                next();
+            },
+        ],
+        (req, res, next) => {
+            console.log("new 5th route handlers!");
+            res.send("new 5th Respose!!");
+            next();
+        },
+    ]
+);
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
