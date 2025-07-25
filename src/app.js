0a106adf5892a694c68a56db6dc1c8dc1c0b5d91
Lakshman76 <lakshmankumar2603@gmail.com>
2025-07-25 20:59:28 +0530

    init database connection

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
+require("dotenv").config();
 const express = require('express');
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
 
-app.use("/getUserData", (req, res, next) => {
-    throw new Error("Bhooo I'm error");
-    res.send("User data");
-})
-
-app.use("/", (err, req, res, next) => {
-    if (err) {
-        console.error(err.message);
-        res.status(500).send("Something went wrong");
+app.post("/signup", async (req, res) => {
+    const userData = {
+        firstName: "sachin",
+        lastName: "tendulkar",
+        emailId: "sachin@example.com",
+        password: "sachin@123"
+    }
+    const user = new User(userData);
+    try {
+        await user.save();
+        res.send("User created successfully");
+    } catch (error) {
+        res.send("Failed to create user", error.message);
     }
 })
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+connectDB().then(() => {
+    console.log("Connected to database");
+    app.listen(3000, () => {
+        console.log("Listening on port 3000");
+    });
+}).catch((err) => {
+    console.log("Failed to connect to database", err);
 });
